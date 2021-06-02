9bf1bf5e021cd2340db95902251ebbc25c979b05
AymenBida <aymen.bida.00@gmail.com>
2021-06-02 11:56:47 +0100

    Add App component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BooksList from './BooksList';
+import BooksForm from './BooksForm';
+
+const App = () => (
+  <div>
+    <BooksList />
+    <BooksForm />
+  </div>
+);
+
+export default App;
