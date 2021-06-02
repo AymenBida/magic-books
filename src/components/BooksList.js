d6e40fdeeae176f088540b17d98758d5c699b34b
AymenBida <aymen.bida.00@gmail.com>
2021-06-02 12:03:49 +0100

    Add BooksList component

---
+++ src/components/BooksList.js
@@ -0,0 +1,21 @@
+const BooksList = () => (
+  <table>
+    <thead>
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>The Alchemist</td>
+        <td>Philosophy</td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+export default BooksList;
