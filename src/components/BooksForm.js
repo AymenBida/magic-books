745c2da4bdd7de049b9485a7eb4f74b146a4b82f
AymenBida <aymen.bida.00@gmail.com>
2021-06-03 19:05:25 +0100

    Add state for BooksForm and handle change in input element and in select element

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 export const CATEGORIES = [
   { name: 'Action', id: 1 },
   { name: 'Biography', id: 2 },
@@ -8,30 +10,43 @@ export const CATEGORIES = [
   { name: 'Sci-Fi', id: 7 },
 ];
 
-const BooksForm = () => (
-  <form>
-    <div>
-      <label htmlFor="title">
-        Title
-        <input id="title" type="text" />
-      </label>
+const BooksForm = () => {
+  const [bookTitle, setBookTitle] = useState('');
+  const [bookCategory, setBookCategory] = useState('1');
 
-    </div>
-    <div>
-      <label htmlFor="category">
-        Category
-        <select name="category" id="category">
-          {CATEGORIES.map(
-            (category) => <option key={category.id} value={category.id}>{category.name}</option>,
-          )}
-        </select>
-      </label>
-    </div>
-    <div>
-      <button type="button">Submit</button>
-    </div>
+  const handleChange = (event) => {
+    setBookTitle(event.target.value);
+  };
 
-  </form>
-);
+  const handleCategory = (event) => {
+    setBookCategory(event.target.value);
+  };
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="title">
+          Title
+          <input id="title" type="text" value={bookTitle} onChange={handleChange} />
+        </label>
+        {console.log(bookCategory)}
+      </div>
+      <div>
+        <label htmlFor="category">
+          Category
+          <select name="category" id="category" value={bookCategory} onChange={handleCategory}>
+            {CATEGORIES.map(
+              (category) => <option key={category.id} value={category.id}>{category.name}</option>,
+            )}
+          </select>
+        </label>
+      </div>
+      <div>
+        <button type="button">Submit</button>
+      </div>
+
+    </form>
+  );
+};
 
 export default BooksForm;
